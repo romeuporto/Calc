91b2b844e61b6f72d2c8a61b3aba72068cd3f082
Romeu Porto <romeu.porto.sms@gmail.com>
2021-03-08 18:10:41 -0300

    finished the text and radius fields, ending the button

--- calc/lib/PageCalc.dart
+++ calc/lib/PageCalc.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PageCalc extends StatefulWidget {
@@ -9,9 +10,9 @@ class _PageCalcState extends State<PageCalc> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal.shade900,
         body: SingleChildScrollView(
           child: Container(
+            color: Colors.teal.shade900,
             padding: EdgeInsets.only(
               top: 100,
               right: 40,
@@ -32,7 +33,7 @@ class _PageCalcState extends State<PageCalc> {
                     ),
                   ),
                   SizedBox(
-                      height: 40,
+                      height: 30,
                   ),
                   TextField(
                     keyboardType: TextInputType.number,
@@ -50,9 +51,48 @@ class _PageCalcState extends State<PageCalc> {
                       ),
                     ),
                   ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      hintText: "Digite o Valor",
+                      hintStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 1.5,
+                            color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: <Widget>[
-                      Text("Soma",
+                      Text(
+                        "Selecione uma operação abaixo:",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text("Soma (+)",
                       style: TextStyle(
                         color: Colors.white,
                         fontWeight: FontWeight.bold,
@@ -63,7 +103,67 @@ class _PageCalcState extends State<PageCalc> {
                         groupValue: null,
                         onChanged: null,
                       ),
+                      Text("Diminuir (-)",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Radio(
+                        value: null,
+                        groupValue: null,
+                        onChanged: null,
+                      ),
                     ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text("Multiplicar (x)",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Radio(
+                        value: null,
+                        groupValue: null,
+                        onChanged: null,
+                      ),
+                      Text("Dividir (/)",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Radio(
+                        value: null,
+                        groupValue: null,
+                        onChanged: null,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  RaisedButton(
+                    padding: EdgeInsets.only(
+                      top: 15,
+                      right: 100,
+                      left: 100,
+                      bottom: 15,
+                    ),
+                    child: Text(
+                      "Cálcular",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 80,
                   ),
                 ],
               ),
