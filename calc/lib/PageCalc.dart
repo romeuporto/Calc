49633af6a1a39e0b082c849c75bbfeb051e364ac
Romeu Porto <romeu.porto.sms@gmail.com>
2021-03-08 13:32:02 -0300

    loagins PageCalc

--- calc/lib/PageCalc.dart
+++ calc/lib/PageCalc.dart
@@ -9,15 +9,67 @@ class _PageCalcState extends State<PageCalc> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Container(
-
-            )
-          ],
+      backgroundColor: Colors.teal.shade900,
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.only(
+              top: 100,
+              right: 40,
+              left: 40,
+            ),
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Text(
+                    "Calc",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                      height: 40,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      hintText: "Digite o Valor",
+                      hintStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 1.5,
+                            color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text("Soma",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      ),
+                      Radio(
+                        value: null,
+                        groupValue: null,
+                        onChanged: null,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
-      ),
-    );
+      );
   }
 }
