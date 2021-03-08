d0069cbcf89311ac376d729654aa068a372f75c4
Romeu Porto <romeu.porto.sms@gmail.com>
2021-03-08 12:12:09 -0300

    project started with the children of the home page

---
+++ calc/lib/PageCalc.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class PageCalc extends StatefulWidget {
+  @override
+  _PageCalcState createState() => _PageCalcState();
+}
+
+class _PageCalcState extends State<PageCalc> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ calc/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'PageCalc.dart';
+
+
+void main(){
+  runApp(MaterialApp(
+    home: PageCalc(),
+  ));
+}
